f7c758577d40720a37e4c06a65de9ffaefc44665
radwa <mail2radwa@gmail.com>
2024-08-02 19:24:29 +0200

    chore: Setup user logging

--- js/main.js
+++ js/main.js
@@ -1,4 +1,72 @@
-axios.get('https://tarmeezacademy.com/api/v1/posts').then(response => {
+const baseUrl = 'https://tarmeezacademy.com/api/v1';
+const loginBtn = document.getElementById('login-btn');
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+const invalid = document.getElementById('invalid');
+const loginModal = document.getElementById('login-modal');
+const logoutBtn = document.getElementById('logout');
+const alertPlaceholder = document.getElementById('alert');
+const defaultUserImg = `
+<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="#bbb" class="bi bi-person-fill border border-1 rounded-circle" viewBox="0 0 16 16">
+  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+</svg>`;
+
+
+const hideModal = modal => {
+  const modalInstance = bootstrap.Modal.getInstance(modal);
+  modalInstance.hide();
+}
+// =============== Build logging UI ============
+// ======================================
+const setUpLogUI = () => {
+  const token = localStorage.getItem('token');
+  const loginBtn = document.getElementById('login');
+  const registerBtn = document.getElementById('register');
+  if (token) {
+    loginBtn.classList.add('hide-me');
+    registerBtn.classList.add('hide-me');
+    logoutBtn.classList.remove('hide-me');
+  } else {
+    loginBtn.classList.remove('hide-me');
+    registerBtn.classList.remove('hide-me');
+    logoutBtn.classList.add('hide-me');
+  }
+}
+
+window.onload = () => setUpLogUI();
+
+// =============== Alerts ============
+// ======================================
+const showAlert = (message, type) => {
+  const appendAlert = () => {
+    const wrapper = document.createElement('div');
+    wrapper.setAttribute('id', 'close-alert')
+    wrapper.innerHTML = [
+      `<svg xmlns="http://www.w3.org/2000/svg" class="d-none">
+        <symbol id="check-circle-fill" viewBox="0 0 16 16">
+        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"></path>
+        </symbol>
+      </svg>
+      <div class="alert alert-${type} alert-dismissible d-flex align-items-center" role="alert">`,
+      `${type === 'success' && `<svg class="bi flex-shrink-0 me-2" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"></use></svg>`}`,
+      `   <div>${message}</div>`,
+      '</div>'
+    ].join('')
+    alertPlaceholder.append(wrapper)
+  }
+  setTimeout(() => {
+    appendAlert(message, type);
+  }, 250)
+  setTimeout(() => {
+    const alertHide = bootstrap.Alert.getOrCreateInstance('#close-alert');
+    alertHide.close()
+  }, 1500)
+  
+
+}
+// =============== get posts ============
+// ======================================
+axios.get(`${baseUrl}/posts`).then(response => {
   const posts = response.data.data;
   const postsContainer = document.getElementById('posts');
 
@@ -7,28 +75,29 @@ axios.get('https://tarmeezacademy.com/api/v1/posts').then(response => {
     for (let tag of tags) {
       content += `
       <li>
-        <button type="button" class="btn btn-secondary bg-secondary">${tag}</button>
+        <button type="button" class="btn btn-secondary bg-secondary">${tag.name}</button>
       </li>`;
     }
     return content;
   };
-
   postsContainer.innerHTML = '';
+
   for(let post of posts) {
+    const userImg = `<img src="${post.author.profile_image}" alt="${post.author.name}" class="border border-1 rounded-circle">`;
     const content = `
       <article class="card my-4 shadow-sm">
       <header>
         <h2 class="card-header d-flex align-items-center gap-2">
-          <img src="${post.author.profile_image}" alt="${post.author.name}" class="border border-1 rounded-circle">${post.author.username}
+        ${post.author.profile_image && typeof post.author.profile_image === 'string'? userImg : defaultUserImg} ${post.author.username}
         </h2>
       </header>
       <div class="card-body">
         <figure>
-          <img src="${post.image}" alt="${post.title}" class="w-100 mw-100">
+          <img src="${post.image || ''}" alt="${post.title || ''}" class="w-100 mw-100">
           <time class="text-body-tertiary fw-medium d-block mt-1">${post.created_at}</time>
           <figcaption class="mt-3">
-            <h3 class="card-title">${post.title ?  post.title : ''}</h3>
-            <p class="card-text">${post.body}</p>
+            <h3 class="card-title">${post.title || ''}</h3>
+            <p class="card-text">${post.body || ''}</p>
           </figcaption>
         </figure>
         <footer class="border-top pt-2 gap-1">
@@ -48,3 +117,54 @@ axios.get('https://tarmeezacademy.com/api/v1/posts').then(response => {
     postsContainer.innerHTML += content;
   }
 })
+
+// =============== get usersName list ============
+// ======================================
+let usersNameList;
+axios.get(`${baseUrl}/users`).then(response => {
+  const users = response.data.data;
+  usersNameList = users.map(user => user.username);
+})
+
+// =============== login ============
+// ======================================
+const isNotEmpty = input => !input.value.trim().length;
+// Disable loginInput if user not fill inputs
+loginBtn.disabled = isNotEmpty(username) || isNotEmpty(password);
+username.addEventListener('input', () => loginBtn.disabled = isNotEmpty(username) || isNotEmpty(password))
+password.addEventListener('input', () => loginBtn.disabled = isNotEmpty(username) || isNotEmpty(password))
+
+// Handle login form validation
+
+
+loginBtn.addEventListener('click', () => {
+
+
+  const params = {
+    'username': username.value.trim(),
+    'password': password.value.trim()
+  };
+
+  axios.post(`${baseUrl}/login`, params)
+  .then(response => {
+    localStorage.setItem('token', response.data.token);
+    localStorage.setItem('user', JSON.stringify(response.data.user));
+    hideModal(loginModal);
+    showAlert('Logged in successfully', 'success');
+    setUpLogUI();
+  })
+
+});
+
+
+
+// =============== logout ============
+// ======================================
+logoutBtn.addEventListener('click', () => {
+
+  localStorage.removeItem('token');
+  localStorage.removeItem('user');
+  showAlert('Logged out successfully', 'success');
+  setUpLogUI();
+});
+setUpLogUI();
