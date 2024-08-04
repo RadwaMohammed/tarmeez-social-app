7eb13321a674aaa285dc558519569b03d7c41da8
radwa <mail2radwa@gmail.com>
2024-08-04 05:03:28 +0200

    chore: Setup register button

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 const baseUrl = 'https://tarmeezacademy.com/api/v1';
+// login modal
 const loginBtn = document.getElementById('login-btn');
 const username = document.getElementById('username');
 const password = document.getElementById('password');
-const invalid = document.getElementById('invalid');
 const loginModal = document.getElementById('login-modal');
+// register modal
+const registerBtn = document.getElementById('register-btn');
+const rUsername = document.getElementById('r-username');
+const rPassword = document.getElementById('r-password');
+const rName = document.getElementById('name');
+const email = document.getElementById('email');
+const registerModal = document.getElementById('register-modal');
+
+const invalid = document.getElementById('invalid');
+
 const logoutBtn = document.getElementById('logout');
 const alertPlaceholder = document.getElementById('alert');
 const defaultUserImg = `
@@ -44,11 +54,14 @@ const showAlert = (message, type) => {
     wrapper.innerHTML = [
       `<svg xmlns="http://www.w3.org/2000/svg" class="d-none">
         <symbol id="check-circle-fill" viewBox="0 0 16 16">
-        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"></path>
+          <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"></path>
+        </symbol>
+        <symbol id="exclamation-triangle-fill" viewBox="0 0 16 16">
+          <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"></path>
         </symbol>
       </svg>
       <div class="alert alert-${type} alert-dismissible d-flex align-items-center" role="alert">`,
-      `${type === 'success' && `<svg class="bi flex-shrink-0 me-2" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"></use></svg>`}`,
+      `<svg class="bi flex-shrink-0 me-2" role="img" aria-label="${type === 'success' ? 'Success:' : 'Danger:'}"><use xlink:href="#${type === 'success' ? 'check-circle' : 'exclamation-triangle'}-fill"></use></svg>`,
       `   <div>${message}</div>`,
       '</div>'
     ].join('')
@@ -129,7 +142,7 @@ axios.get(`${baseUrl}/users`).then(response => {
 // =============== login ============
 // ======================================
 const isNotEmpty = input => !input.value.trim().length;
-// Disable loginInput if user not fill inputs
+// Disable login Button if user not fill inputs
 loginBtn.disabled = isNotEmpty(username) || isNotEmpty(password);
 username.addEventListener('input', () => loginBtn.disabled = isNotEmpty(username) || isNotEmpty(password))
 password.addEventListener('input', () => loginBtn.disabled = isNotEmpty(username) || isNotEmpty(password))
@@ -153,12 +166,54 @@ loginBtn.addEventListener('click', () => {
     showAlert('Logged in successfully', 'success');
     setUpLogUI();
   })
-
 });
 
 
+// =============== Register ============
+// ======================================
+// Disable register button if user not fill inputs
 
-// =============== logout ============
+const isAllowRegiter = () => registerBtn.disabled = isNotEmpty(rUsername) || isNotEmpty(rPassword) || isNotEmpty(rName) || isNotEmpty(email);
+isAllowRegiter();
+rUsername.addEventListener('input', () => {
+  isAllowRegiter();
+  rUsername.value = rUsername.value.trim();
+  console.log(rUsername.value)
+})
+rPassword.addEventListener('input', () => {
+  isAllowRegiter();
+  rPassword.value = rPassword.value.trim();
+})
+rName.addEventListener('input', () => {
+  isAllowRegiter();
+  rName.value = rName.value.trimStart();
+})
+email.addEventListener('input', () => {
+  isAllowRegiter();
+  email.value = email.value.trim();
+})
+
+registerBtn.addEventListener('click', () => {
+  const params = {
+    'username': rUsername.value,
+    'password': rPassword.value,
+    'name': rName.value.replace(/\s+/g, ' ').trim(),
+    'email': email.value
+  };
+
+  axios.post(`${baseUrl}/register`, params)
+  .then(response => {
+    localStorage.setItem('token', response.data.token);
+    localStorage.setItem('user', JSON.stringify(response.data.user));
+    hideModal(registerModal);
+    showAlert('Registered successfully', 'success');
+    setUpLogUI();
+    
+  }).catch(e => showAlert(e.response.data.message, 'danger'))
+});
+
+
+// =============== logout ============register-modal
 // ======================================
 logoutBtn.addEventListener('click', () => {
 
